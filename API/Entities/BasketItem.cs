638fb6e4388a17c2993b54e36f04a6045dde092d
Gabegi <gabriel_pirastru@hotmail.fr>
2025-07-04 17:22:31 +0600

    basket entity

--- API/Entities/BasketItem.cs
+++ API/Entities/BasketItem.cs
@@ -1,8 +1,16 @@
 ﻿namespace API.Entities
 {
-    public class BasketItems
+    public class BasketItem
     {
         public int Id { get; init; }
-        public int Quantity { get; set; }   
+        public int Quantity { get; set; }
+
+
+        // Foreign Keys
+        public int ProductId { get; set; }
+        public required Product Product {get;set;}
+
+        public int BasketId { get; set; }
+        public Basket Basket { get; set; }
     }
 }
