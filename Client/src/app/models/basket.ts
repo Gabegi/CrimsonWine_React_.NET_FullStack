590b9a59e38661286a04fe3212dc821ca3c1de6d
Gabegi <gabriel_pirastru@hotmail.fr>
2025-07-07 10:52:54 +0600

    basket model

---
+++ Client/src/app/models/basket.ts
@@ -0,0 +1,16 @@
+export type Basket = {
+  basketId: string;
+  items: Item[];
+  clientSecret?: string;
+  paymentIntentId?: string;
+};
+
+export type Item = {
+  productId: number;
+  name: string;
+  price: number;
+  pictureUrl: string;
+  brand: string;
+  type: string;
+  quantity: number;
+};
