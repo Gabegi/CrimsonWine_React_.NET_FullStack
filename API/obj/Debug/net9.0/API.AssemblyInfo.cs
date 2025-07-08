3dc7856e9f586a8cfb349b62d7bab93daeadd079
Gabegi <gabriel_pirastru@hotmail.fr>
2025-07-08 10:33:04 +0600

    basket component

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfbe141b38e6946ed75dc1bff8e878a5cadbf2e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8af67415b24dbdc75f1b0de84173efcd343f5e65")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
