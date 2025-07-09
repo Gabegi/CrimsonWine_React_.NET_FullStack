5a2a2ff66554e677135e70ebe132fa6d3a94afed
Gabegi <gabriel_pirastru@hotmail.fr>
2025-07-09 23:53:51 +0600

    cors

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fb1816021483abd601158bb971d188739d6434e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8613b6c7ffc5302ecdf7748ff75d58982a61450")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
