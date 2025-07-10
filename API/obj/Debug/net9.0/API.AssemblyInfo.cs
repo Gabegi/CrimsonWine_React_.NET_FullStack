55dbb17adcd758046085f4229864e46db7d933eb
Gabegi <gabriel_pirastru@hotmail.fr>
2025-07-10 10:23:12 +0600

    client api

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e95cc42249b28127bbccfccb74719ed6fdbd4dc0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+941215ad4e5cc80f571ca9d23fab79677e90fb25")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
